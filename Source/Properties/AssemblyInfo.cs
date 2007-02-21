529e9348228cb008a0320b67bbf5862e1214b5bd
Daniel Cazzulino <daniel@cazzulino.com>
2007-02-21 19:51:51 +0000

    Added assembly-level attributes.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,35 +1,33 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Mvp.Xml")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mvp.Xml")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+
+[assembly: AssemblyTitle(ThisAssembly.Title)]
+[assembly: AssemblyDescription(ThisAssembly.Description)]
+[assembly: AssemblyCompany(ThisAssembly.Company)]
+[assembly: AssemblyCopyright(ThisAssembly.Copyright)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+#if Debug
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: ComVisible(false)]
 [assembly: Guid("74262cce-9f30-418c-8e28-35ae800c9b0e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+internal static partial class ThisAssembly
+{
+	public const string Title = "Mvp.Xml";
+	public const string Description = "MVP XML Library";
+	public const string Product = "Mvp.Xml";
+	public const string Company = "MVP XML Project";
+	public const string Copyright = "";
+}
