0764a6294da8c6706406a4be357bb7b6d5f9c58e
Daniel Cazzulino <daniel@cazzulino.com>
2007-02-28 04:32:47 +0000

    Added tests for XPathDocumentWriter and removed XpathDocumentHelper class.

---
+++ Source/Common/XPath/XPathDocumentWriter.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Xml.XPath;
+using System.Xml;
+using System.IO;
+using System.Diagnostics;
+
+namespace Mvp.Xml.Common.XPath
+{
+	/// <summary>
+	/// <see cref="XmlWriter"/> that can produce an 
+	/// <see cref="XPathDocument"/> without the need 
+	/// to parse XML.
+	/// </summary>
+	/// <remarks>
+	/// Using this writer whenever you need to perform 
+	/// fast chained transformations. Using this class 
+	/// avoids reparsing of the intermediate output, 
+	/// yielding lower memory and CPU usage.
+	/// <para>
+	/// Future versions of <see cref="XPathDocument"/> may 
+	/// offer this functionality out of the box.
+	/// </para>
+	/// <para>
+	/// <b>Important: </b> this class requires unrestricted 
+	/// member access reflection permissions in order to run. 
+	/// If the Mvp.Xml assembly is installed in the GAC, it will 
+	/// run without problems. The Mvp.Xml allows partially trusted 
+	/// callers, so only this assembly needs to be GAC'ed. 
+	/// </para>
+	/// <para>
+	/// Alternatively, you can configure .NET security policy 
+	/// to allow the appropriate permission (see <see cref="ReflectionPermission"/>, 
+	/// <see cref="PermissionState.Unrestricted"/> and 
+	/// <see cref="ReflectionPermissionFlag.MemberAccess"/>).
+	/// </para>
+	/// </remarks>
+	public class XPathDocumentWriter : XmlWrappingWriter
+	{
+		static ConstructorInfo defaultConstructor;
+		static MethodInfo loadWriterMethod;
+
+		XPathDocument document;
+		bool hasRoot;
+
+		static XPathDocumentWriter()
+		{
+			ReflectionPermission perm = new ReflectionPermission(PermissionState.Unrestricted);
+			perm.Flags = ReflectionPermissionFlag.MemberAccess;
+
+			try
+			{
+				perm.Assert();
+
+				Type t = typeof(XPathDocument);
+				defaultConstructor = t.GetConstructor(
+					BindingFlags.NonPublic | BindingFlags.Instance, null,
+					Type.EmptyTypes,
+					new ParameterModifier[0]);
+				Debug.Assert(defaultConstructor != null, ".NET Framework implementation changed");
+
+				loadWriterMethod = t.GetMethod("LoadFromWriter", BindingFlags.Instance | BindingFlags.NonPublic);
+				Debug.Assert(loadWriterMethod != null, ".NET Framework implementation changed");
+			}
+			finally
+			{
+				ReflectionPermission.RevertAssert();
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="XPathDocumentWriter"/>.
+		/// </summary>
+		public XPathDocumentWriter() : this(String.Empty) { }
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="XPathDocumentWriter"/> using 
+		/// the given <paramref name="baseUri"/>.
+		/// </summary>
+		/// <param name="baseUri">Base URI to used to construct the <see cref="XPathDocument"/>.</param>
+		public XPathDocumentWriter(string baseUri)
+			: base(XmlWriter.Create(new StringWriter()))
+		{
+			Guard.ArgumentNotNull(baseUri, "baseUri");
+
+			document = CreateDocument();
+			base.BaseWriter = GetWriter(document, baseUri);
+		}
+
+		/// <summary>
+		/// See <see cref="XmlWriter.WriteStartElement(string, string, string)"/>.
+		/// </summary>
+		public override void WriteStartElement(string prefix, string localName, string ns)
+		{
+			base.WriteStartElement(prefix, localName, ns);
+			if (!hasRoot) hasRoot = true;
+		}
+
+		/// <summary>
+		/// Closes the writer and retrieves the created <see cref="XPathDocument"/>.
+		/// </summary>
+		public new XPathDocument Close()
+		{
+			if (!hasRoot) throw new XmlException(Properties.Resources.Xml_MissingRoot);
+
+			base.Close();
+			return document;
+		}
+
+		private static XPathDocument CreateDocument()
+		{
+			return (XPathDocument)defaultConstructor.Invoke(new object[0]);
+		}
+
+		private static XmlWriter GetWriter(XPathDocument document, string baseUri)
+		{
+			return (XmlWriter)loadWriterMethod.Invoke(document, new object[] { 0, baseUri });
+		}
+	}
+}
---
+++ UnitTests/Common/XPathDocumentWriterFixture.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using Mvp.Xml.Common.XPath;
+using System.IO;
+
+#if !NUNIT
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+#endif
+
+namespace Mvp.Xml.Tests.Common
+{
+	[TestClass]
+	public class XPathDocumentWriterFixture
+	{
+		[TestMethod]
+		public void ShouldCreateXmlWriterForDocument()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter();
+			Assert.IsNotNull(writer);
+		}
+
+		[TestMethod]
+		public void ShouldAllowEmptyBaseUri()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter(String.Empty);
+			Assert.IsNotNull(writer);
+		}
+
+		[ExpectedException(typeof(ArgumentNullException))]
+		[TestMethod]
+		public void ShouldThrowWithNullBaseUri()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter((string)null);
+		}
+
+		[ExpectedException(typeof(XmlException))]
+		[TestMethod]
+		public void ShouldThrowIfNoRootWritten()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter();
+			XPathDocument doc = writer.Close();
+		}
+
+		[TestMethod]
+		public void ShouldAcceptStartElementRootOnly()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter();
+			writer.WriteStartElement("Foo");
+			XPathDocument doc = writer.Close();
+		}
+
+		[TestMethod]
+		public void ShouldWriteRootElement()
+		{
+			XPathDocumentWriter writer = new XPathDocumentWriter();
+			writer.WriteElementString("hello", "world");
+			XPathDocument doc = writer.Close();
+
+			Assert.IsNotNull(doc);
+
+			string xml = GetXml(doc);
+
+			Assert.AreEqual("<hello>world</hello>", xml);
+		}
+
+		private static string GetXml(XPathDocument doc)
+		{
+			XPathNavigator nav = doc.CreateNavigator();
+			StringWriter sw = new StringWriter();
+			XmlWriterSettings set = new XmlWriterSettings();
+			set.OmitXmlDeclaration = true;
+			using (XmlWriter w = XmlWriter.Create(sw, set))
+			{
+				nav.WriteSubtree(w);
+			}
+
+			return sw.ToString();
+		}
+
+		// Uses nametable to atomize strings
+		// Checks root element on close.
+
+	}
+}
