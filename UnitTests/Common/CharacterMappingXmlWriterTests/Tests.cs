22f54d3019085b73a0ed68e3bae0db0607428dfa
Oleg Tkachenko <olegtk@microsoft.com>
2007-06-19 20:22:22 +0000

    Finished CharacterMappingXmlWriter.

--- UnitTests/Common/CharacterMappingXmlWriterTests/Tests.cs
+++ UnitTests/Common/CharacterMappingXmlWriterTests/Tests.cs
@@ -39,5 +39,68 @@ namespace Mvp.Xml.Tests.CharacterMappingXmlWriterTests
             writer.Close();
             Assert.IsTrue(sw.ToString() == "<foo>FOOgh</foo>");
         }
+
+        [TestMethod]
+        public void TestShouldReplaceInText2() {
+            Dictionary<char, string> mapping = new Dictionary<char, string>();
+            mapping.Add('f', "FOO");
+            mapping.Add('z', "ZzZ");
+            StringWriter sw = new StringWriter();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            settings.Indent = false;
+            CharacterMappingXmlWriter writer = new CharacterMappingXmlWriter(XmlWriter.Create(sw, settings), mapping);
+            writer.WriteElementString("foo", "abcd z efgh f zzz.");
+            writer.Close();
+            Assert.IsTrue(sw.ToString() == "<foo>abcd ZzZ eFOOgh FOO ZzZZzZZzZ.</foo>");
+        }
+
+        [TestMethod]
+        public void TestShouldReplaceInAttribute() {
+            Dictionary<char, string> mapping = new Dictionary<char, string>();
+            mapping.Add('f', "FOO");
+            StringWriter sw = new StringWriter();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            settings.Indent = false;
+            CharacterMappingXmlWriter writer = new CharacterMappingXmlWriter(XmlWriter.Create(sw, settings), mapping);
+            writer.WriteStartElement("foo");
+            writer.WriteAttributeString("bar", "fghj");
+            writer.WriteEndElement();
+            writer.Close();
+            Assert.IsTrue(sw.ToString() == "<foo bar=\"FOOghj\" />");
+        }
+
+        [TestMethod]
+        public void TestShouldNotEscape() {
+            Dictionary<char, string> mapping = new Dictionary<char, string>();
+            mapping.Add('(', "<");
+            mapping.Add(')', ">");
+            StringWriter sw = new StringWriter();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            settings.Indent = false;
+            CharacterMappingXmlWriter writer = new CharacterMappingXmlWriter(XmlWriter.Create(sw, settings), mapping);
+            writer.WriteElementString("foo", "(%= bar%)");
+            writer.Close();            
+            Assert.IsTrue(sw.ToString() == "<foo><%= bar%></foo>");
+        }
+
+        [TestMethod]
+        public void TestShouldNotEscapeInAttribute() {
+            Dictionary<char, string> mapping = new Dictionary<char, string>();
+            mapping.Add('(', "<");
+            mapping.Add(')', ">");
+            StringWriter sw = new StringWriter();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = true;
+            settings.Indent = false;
+            CharacterMappingXmlWriter writer = new CharacterMappingXmlWriter(XmlWriter.Create(sw, settings), mapping);
+            writer.WriteStartElement("foo");
+            writer.WriteAttributeString("bar", "(%= bar%)");
+            writer.WriteEndElement();
+            writer.Close();
+            Assert.IsTrue(sw.ToString() == "<foo bar=\"<%= bar%>\" />");
+        }
     }
 }
